c754d86852c321435bff75a9b7de21d730bc344a
Blake Kaufman <blakekaufman9@gmail.com>
2024-08-16 13:36:25 -0500

    started creating payment page

--- src/pages/paymentPage/index.js
+++ src/pages/paymentPage/index.js
@@ -1,6 +1,22 @@
+import QRCode from "react-qr-code";
+import icons from "../../constants/icons";
+import "./style.css";
+
 export default function PaymentPage() {
+  console.log(icons.Logo);
   return (
-    <div>
+    <div className="PaymentPage-Container">
+      <div className="PaymentPage-QRcontainer">
+        <div className="PaymentPage-QRPadding">
+          <QRCode
+            xlinkTitle="test"
+            className="PaymentPage-QR"
+            bgColor="white"
+            color="#262626"
+            value="Genrating QR Code"
+          />
+        </div>
+      </div>
       <p>Testing</p>
     </div>
   );
