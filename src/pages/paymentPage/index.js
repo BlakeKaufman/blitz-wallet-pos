67a2f7816851259bd28cbba3b69d1896d08850e2
Blake Kaufman <blakekaufman9@gmail.com>
2024-08-17 10:43:46 -0400

    continue building out payment page

--- src/pages/paymentPage/index.js
+++ src/pages/paymentPage/index.js
@@ -1,23 +1,59 @@
-import QRCode from "react-qr-code";
+// import QRCode from "react-qr-code";
+import QRCode from "qrcode.react";
 import icons from "../../constants/icons";
 import "./style.css";
+import { copyTextToClipboard } from "../../functions/copyToClipboard";
+import Popup from "reactjs-popup";
+import CopyToCliboardPopup from "../../components/popup";
 
 export default function PaymentPage() {
   console.log(icons.Logo);
   return (
     <div className="PaymentPage-Container">
-      <div className="PaymentPage-QRcontainer">
+      <button className="PaymentPage-QRcontainer">
         <div className="PaymentPage-QRPadding">
           <QRCode
-            xlinkTitle="test"
-            className="PaymentPage-QR"
-            bgColor="white"
-            color="#262626"
-            value="Genrating QR Code"
+            size={200}
+            imageSettings={{
+              src: icons.Logo,
+              height: 50,
+              width: 50,
+              excavate: true,
+            }}
+            value="testing"
+            renderAs="canvas"
           />
         </div>
+      </button>
+      <div className="QR-OptionsContainer">
+        <button className="QR-Option">Edit</button>
+        <Popup
+          trigger={
+            <button
+              onClick={() => {
+                copyTextToClipboard("Testing");
+              }}
+              className="QR-Option"
+            >
+              {" "}
+              Copy
+            </button>
+          }
+          modal
+        >
+          {(close) => <CopyToCliboardPopup close={close} />}
+        </Popup>
+        {/* <button
+          onClick={() => {
+            copyTextToClipboard("Testing");
+          }}
+          className="QR-Option"
+        >
+          Copy
+        </button> */}
       </div>
-      <p>Testing</p>
+      <button className="QR-OptionNoFill QR-Option">Choose format</button>
+      {/* <PopupComponenet popupContent="test" /> */}
     </div>
   );
 }
