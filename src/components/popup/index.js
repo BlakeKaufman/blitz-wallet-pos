4b7096c975d9d34eaaf033aaa99111e547666348
Blake Kaufman <blakekaufman9@gmail.com>
2024-08-17 11:42:39 -0400

    added copy functioanlity to popup

--- src/components/popup/index.js
+++ src/components/popup/index.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./style.css";
+import { copyTextToClipboard } from "../../functions/copyToClipboard";
 
-export default function CopyToCliboardPopup({ close }) {
+export default function CopyToCliboardPopup({ close, content }) {
+  useEffect(() => {
+    copyTextToClipboard(content);
+  }, []);
   return (
     <div className="Popup-Container">
       <p className="Popup-Text">Text copied to clipboard</p>
