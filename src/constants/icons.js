af1dc7646ddf64422285e695c09c3529f38ad43e
Blake Kaufman <blakekaufman9@gmail.com>
2024-08-17 11:42:06 -0400

    added arrow icon

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -1,4 +1,5 @@
 import LNicon from "../assets/icons/LNicon.png";
 import Logo from "../logo.png";
+import leftArrow from "../assets/icons/arrowLeft.png";
 
-export default { LNicon, Logo };
+export default { LNicon, Logo, leftArrow };
