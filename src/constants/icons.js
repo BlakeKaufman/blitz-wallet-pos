2ebbcc7f55c537f0075a3c8370a0a8fd5e70f68d
Blake Kaufman <blakekaufman9@gmail.com>
2024-08-18 13:43:33 -0400

    added lightning and liquid icon assets

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -2,5 +2,14 @@ import LNicon from "../assets/icons/LNicon.png";
 import Logo from "../logo.png";
 import leftArrow from "../assets/icons/arrowLeft.png";
 import CheckMark from "../assets/icons/checkMark.png";
+import LiquidIcon from "../assets/icons/liquidIcon.svg";
+import LightningIcon from "../assets/icons/LightningIcon.svg";
 
-export default { LNicon, Logo, CheckMark, leftArrow };
+export default {
+  LNicon,
+  Logo,
+  CheckMark,
+  leftArrow,
+  LiquidIcon,
+  LightningIcon,
+};
