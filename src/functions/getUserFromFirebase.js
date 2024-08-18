0e1c680dbd8a8bf7f6cf0a0c70e21aa21ddf03ca
Blake Kaufman <blakekaufman9@gmail.com>
2024-08-18 10:31:16 -0400

    updaeted to env

--- src/functions/getUserFromFirebase.js
+++ src/functions/getUserFromFirebase.js
@@ -26,7 +26,7 @@ import {
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBKn2evN91MVTfrAQgL23y1v8gHcVO6oU8",
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: "blitz-wallet-82b39.firebaseapp.com",
   projectId: "blitz-wallet-82b39",
   storageBucket: "blitz-wallet-82b39.appspot.com",
