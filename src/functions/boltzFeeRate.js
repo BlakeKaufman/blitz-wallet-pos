1a97df58867c911e4365302c2cb237a667e1dd83
Blake Kaufman <blakekaufman9@gmail.com>
2024-08-18 12:08:30 -0400

    fixed feerate function to work in both environment

--- src/functions/boltzFeeRate.js
+++ src/functions/boltzFeeRate.js
@@ -3,7 +3,9 @@ import { getBoltzApiUrl } from "./boltz";
 export default async function getBoltzFeeRates() {
   try {
     const response = await (
-      await fetch(`${getBoltzApiUrl("liquid")}/v2/chain/fees`)
+      await fetch(
+        `${getBoltzApiUrl(process.env.REACT_APP_ENVIRONMENT)}/v2/chain/fees`
+      )
     ).json();
     return response["L-BTC"];
   } catch (err) {
