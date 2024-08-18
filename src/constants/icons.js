22558cfcfc46c4748d3708705600d16f757dd3dc
Blake Kaufman <blakekaufman9@gmail.com>
2024-08-18 12:55:27 -0400

    misc changes

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -1,5 +1,6 @@
 import LNicon from "../assets/icons/LNicon.png";
 import Logo from "../logo.png";
 import leftArrow from "../assets/icons/arrowLeft.png";
+import CheckMark from "../assets/icons/checkMark.png";
 
-export default { LNicon, Logo, leftArrow };
+export default { LNicon, Logo, CheckMark, leftArrow };
--- src/functions/getBitcoinPrice.js
+++ src/functions/getBitcoinPrice.js
@@ -1,9 +1,13 @@
 export default async function getBitcoinPrice({ denomination }) {
-  console.log;
-  const response = await fetch(
-    `https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=${denomination.toLowerCase()}`
-  );
-  const data = await response.json();
-  const bitcoinPrice = data.bitcoin.usd;
-  return bitcoinPrice;
+  try {
+    const response = await fetch(
+      `https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=${denomination.toLowerCase()}`
+    );
+    const data = await response.json();
+    const bitcoinPrice = data.bitcoin.usd;
+    return bitcoinPrice;
+  } catch (err) {
+    console.log(err);
+    return 60000;
+  }
 }
